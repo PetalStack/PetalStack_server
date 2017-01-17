819a3ff4054b2a34e2e010404f8220e1c8e28c98
João Henrique Pereira de Almeida <joaohenrique.pereira.almeida@gmail.com>
2017-01-17 20:32:37 -0200

    Add project

---
+++ db/migrate/20170117202622_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
